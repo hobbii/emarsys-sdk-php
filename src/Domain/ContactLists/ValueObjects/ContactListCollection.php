e693d81aac20a9b4bd31186deabe1f814d610c4a
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2025-10-27 12:24:50 +0100

    feat: introduced DataCollection abstract class

--- src/Domain/ContactLists/ValueObjects/ContactListCollection.php
+++ src/Domain/ContactLists/ValueObjects/ContactListCollection.php
@@ -4,46 +4,17 @@ declare(strict_types=1);
 
 namespace Hobbii\Emarsys\Domain\ContactLists\ValueObjects;
 
-use Illuminate\Support\Collection;
-use InvalidArgumentException;
+use Hobbii\Emarsys\Domain\ValueObjects\DataCollection;
 
 /**
  * ContactList collection
  *
- * @extends Collection<int,ContactList>
+ * @extends DataCollection<int,ContactList>
  */
-final class ContactListCollection extends Collection
+final class ContactListCollection extends DataCollection
 {
-    /**
-     * @param  array<int,ContactList>  $items
-     */
-    public function __construct(array $items = [])
+    protected static function getItemClass(): string
     {
-        $this->validateItems($items);
-        parent::__construct($items);
-    }
-
-    /**
-     * @param  array<int,ContactList>  $items
-     */
-    private function validateItems(array $items): void
-    {
-        foreach ($items as $item) {
-            if (! $item instanceof ContactList) {
-                throw new InvalidArgumentException('All items must be instances of ContactList');
-            }
-        }
-    }
-
-    /**
-     * Create a collection from data.
-     *
-     * @param  array<int,array<string,mixed>|ContactList>  $data
-     */
-    public static function from(array $data): self
-    {
-        $contactLists = array_map(fn ($item) => $item instanceof ContactList ? $item : ContactList::from($item), $data);
-
-        return new self($contactLists);
+        return ContactList::class;
     }
 }
