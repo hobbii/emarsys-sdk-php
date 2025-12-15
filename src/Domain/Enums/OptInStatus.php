d3b6413a67d73f1374e7f28d1c07e236cd41fb77
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2025-12-15 14:30:50 +0100

    feat: added helper methods to the Enum

--- src/Domain/Enums/OptInStatus.php
+++ src/Domain/Enums/OptInStatus.php
@@ -9,6 +9,16 @@ enum OptInStatus: int
     case TRUE = 1;
     case FALSE = 2;
 
+    public function isTrue(): bool
+    {
+        return $this === self::TRUE;
+    }
+
+    public function isFalse(): bool
+    {
+        return $this === self::FALSE;
+    }
+
     public function label(): string
     {
         return $this->value === self::TRUE->value ? 'Yes' : 'No';
