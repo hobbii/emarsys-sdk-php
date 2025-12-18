d2e680f571a1e23cfd4b304ea79785be106ed5bf
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2025-12-18 13:59:23 +0100

    feat: implement ResponseInterface in the UpdateContactsResponse

--- src/Domain/Contacts/UpdateContacts/UpdateContactsResponse.php
+++ src/Domain/Contacts/UpdateContacts/UpdateContactsResponse.php
@@ -4,42 +4,48 @@ declare(strict_types=1);
 
 namespace Hobbii\Emarsys\Domain\Contacts\UpdateContacts;
 
+use Hobbii\Emarsys\Domain\Contracts\ResponseInterface;
+use Hobbii\Emarsys\Domain\Contracts\WithErrorsInterface;
+use Hobbii\Emarsys\Domain\Traits\WithErrors;
+use Hobbii\Emarsys\Domain\Traits\WithReply;
 use Hobbii\Emarsys\Domain\ValueObjects\ErrorObject;
+use Hobbii\Emarsys\Domain\ValueObjects\Reply;
 use Hobbii\Emarsys\Domain\ValueObjects\Response;
 use InvalidArgumentException;
 
-final readonly class UpdateContactsResponse
+final readonly class UpdateContactsResponse implements ResponseInterface, WithErrorsInterface
 {
+    use WithErrors;
+    use WithReply;
+
     /**
      * @param  array<int,string>  $ids  The list of IDs of the contacts that were updated
-     * @param  ErrorObject[]  $errors  The details of any contacts not updated, expressed as an array that contains the error code and reason
      */
-    public function __construct(
+    private function __construct(
+        public Reply $reply,
         public array $ids,
-        public array $errors
+        public ?array $errors
     ) {}
-
-    public function hasErrors(): bool
-    {
-        return ! empty($this->errors);
-    }
 
     public static function fromResponse(Response $response): self
     {
         $responseData = $response->dataAsArray();
+        $ids = $responseData['ids'] ?? null;
 
-        if (! isset($responseData['ids'])) {
+        if (! isset($ids)) {
             throw new InvalidArgumentException('Missing "ids" in data response');
         }
 
-        $errors = [];
+        $errors = $responseData['errors'] ?? null;
 
-        foreach ($responseData['errors'] ?? [] as $key => $errorData) {
-            foreach ($errorData as $errorCode => $errorMessage) {
-                $errors[] = new ErrorObject((string) $key, (int) $errorCode, $errorMessage);
-            }
+        if (is_array($errors)) {
+            $errors = array_map(ErrorObject::fromArray(...), $errors);
         }
 
-        return new self($responseData['ids'], $errors);
+        return new self(
+            reply: $response->reply,
+            ids: $ids,
+            errors: $errors
+        );
     }
 }
