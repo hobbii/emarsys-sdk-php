ff30fa4bb427e255cee8629075a4fa517de0e03e
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2025-12-19 10:56:03 +0100

    added docs

--- src/Domain/Enums/ContactSpecialKeyId.php
+++ src/Domain/Enums/ContactSpecialKeyId.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Hobbii\Emarsys\Domain\Enums;
 
+/**
+ * The key is only used for Contact identification in certain API calls.
+ *
+ * Usage:
+ *  $keyId = ContactSpecialKeyId::id->name;  // 'id'
+ *  $keyUid = ContactSpecialKeyId::uid->name; // 'uid'
+ */
 enum ContactSpecialKeyId
 {
     case id;
--- src/Domain/Enums/OptInStatus.php
+++ src/Domain/Enums/OptInStatus.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Hobbii\Emarsys\Domain\Enums;
 
+/**
+ * Emarsys Opt-In Status.
+ * Values are used in contact opt-in fields in Emarsys as option ids.
+ *
+ * Usage:
+ *     $status = OptInStatus::True; // OptInStatus enum instance
+ *     $optionId = OptInStatus::True->value; // 1
+ *     $bool = OptInStatus::True->toBool(); // true
+ */
 enum OptInStatus: int
 {
     case True = 1;
